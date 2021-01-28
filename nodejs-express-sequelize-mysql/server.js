ca3ce8c1cb0e49e8720c6127bdc37af199cbc950
Felipe Portillo Delgado <felipe.portillo@workdoc.com.br>
2021-01-28 14:40:30 -0300

    react

--- nodejs-express-sequelize-mysql/server.js
+++ nodejs-express-sequelize-mysql/server.js
@@ -27,6 +27,8 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to Felipe application." });
 });
 
+require("./app/routes/turorial.routes")(app);
+
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
---
+++ react-crud/src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class App extends Component {
+  render() {
+    return (
+        <div>
+          <nav className="navbar navbar-expand navbar-dark bg-dark">
+            <a href="/tutorials" className="navbar-brand">
+              bezKoder
+            </a>
+            <div className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <Link to={"/tutorials"} className="nav-link">
+                  Tutorials
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to={"/add"} className="nav-link">
+                  Add
+                </Link>
+              </li>
+            </div>
+          </nav>
+  
+          <div className="container mt-3">
+            <Switch>
+              <Route exact path={["/", "/tutorials"]} component={TutorialsList} />
+              <Route exact path="/add" component={AddTutorial} />
+              <Route path="/tutorials/:id" component={Tutorial} />
+            </Switch>
+          </div>
+        </div>
+      );
+  }
+}
+
+export default App;
